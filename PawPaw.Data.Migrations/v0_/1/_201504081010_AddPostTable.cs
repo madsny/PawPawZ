544fc10c2a86d079b0585c1f7ce00626ab56127f
madsny <nyborg.mads@gmail.com>
2015-04-08 10:40:51 +0200

    empty projects

---
+++ PawPaw.Data.Migrations/v0_/1/_201504081010_AddPostTable.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentMigrator;
+
+namespace PawPaw.Data.Migrations.v0_._1
+{
+    [Migration(201504081010)]
+    public class _201504081010_AddPostTable : Migration
+    {
+        public override void Up()
+        {
+            Create.Table("Post")
+                .WithColumn("Id").AsInt32().Identity().PrimaryKey()
+                .WithColumn("Body").AsString(int.MaxValue).Nullable();
+        }
+
+        public override void Down()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
