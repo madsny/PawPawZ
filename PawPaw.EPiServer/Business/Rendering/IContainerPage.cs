d6bc825c1cb1472c1decc691c068e0d0724a5527
Lars Skjelbek <lars.skjelbek@gmail.com>
2015-04-10 11:12:16 +0200

    Committed twice as many projects as intended

--- PawPaw.EPiServer/Business/Rendering/IContainerPage.cs
+++
@@ -1,9 +0,0 @@
-namespace PawPaw.EPiServer.Business.Rendering
-{
-    /// <summary>
-    /// Marker interface for content types which should not be handled by DefaultPageController.
-    /// </summary>
-    interface IContainerPage
-    {
-    }
-}
--- PawPaw.EPiServer/Models/Pages/IHasRelatedContent.cs
+++
@@ -1,9 +0,0 @@
-using EPiServer.Core;
-
-namespace PawPaw.EPiServer.Models.Pages
-{
-    public interface IHasRelatedContent
-    {
-        ContentArea RelatedContentArea { get; }
-    }
-}
