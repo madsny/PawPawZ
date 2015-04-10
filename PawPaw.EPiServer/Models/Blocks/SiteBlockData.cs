5c1b05af8a03b7929b8e06541cf67a70f38616a2
Lars Skjelbek <lars.skjelbek@gmail.com>
2015-04-10 11:06:31 +0200

    Added Alloy EPiServer project

---
+++ PawPaw.EPiServer/Models/Blocks/SiteBlockData.cs
@@ -0,0 +1,12 @@
+
+using EPiServer.Core;
+
+namespace PawPaw.EPiServer.Models.Blocks
+{
+    /// <summary>
+    /// Base class for all block types on the site
+    /// </summary>
+    public abstract class SiteBlockData : BlockData
+    {
+    }
+}
