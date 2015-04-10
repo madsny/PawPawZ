04bf78e25c04dc2d0ad4dcb5de2c90c74d160573
Thomas Svensen <thomas.svensen@bekk.no>
2015-04-10 16:18:36 +0200

    Added first test - now failing to make sure CI build breaks

---
+++ PawPaw.Tests/PostWriterTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using FluentAssertions;
+using Moq;
+using NUnit.Framework;
+using PawPaw.Core;
+
+namespace PawPaw.Tests
+{
+    [TestFixture]
+    public class PostWriterTests
+    {
+        private Mock<IPostRepository> _mockPostRepo;
+        private Mock<ICommentRepository> _mockCommentRepo;
+
+        [SetUp]
+        public void Setup()
+        {
+            _mockPostRepo = new Mock<IPostRepository>();
+            _mockCommentRepo = new Mock<ICommentRepository>();
+        }
+
+        [Test]
+        public void CreatePost_NoContent_ShouldFail()
+        {
+            // arrange
+            var postWriter = new PostWriter(_mockPostRepo.Object, _mockCommentRepo.Object);
+
+            // act
+            Action action = () =>
+                postWriter.CreatePost(new Post(), null);
+            
+            // assert
+            action.ShouldThrow<Exception>();
+        }
+         
+    }
+}
