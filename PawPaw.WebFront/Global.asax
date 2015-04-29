fe882ba5ac9efe563126a209f7d2b34ea6e3e3ec
madsny <nyborg.mads@gmail.com>
2015-04-29 15:55:24 +0200

    something in React

---
+++ PawPaw.WebFront/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PawPaw.WebFront.Global" Language="C#" %>
---
+++ PawPaw.WebFront/Js/app/Groups.js
@@ -0,0 +1,24 @@
+﻿var Groups = React.createClass({
+    getInitialState: function(){
+        return {
+            data:[{Name:"hei", Id: 1}, {Name:"lei", Id: 2}]
+        };
+    },
+    render: function() {
+        var groups = this.state.data.map(function(group){
+            return (<li>{group.Name}</li>);
+        });
+        
+        return (
+            <ul>
+                {groups}
+            </ul>
+        );
+    }
+});
+
+
+React.render(
+    <Groups></Groups>,
+    document.getElementById("content")
+    );
